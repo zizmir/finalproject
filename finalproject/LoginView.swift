fd08b00cc0d9aea152a42332d2a0cb725e18a98d
m'bark <mbark.bouznir@gmail.com>
2018-02-16 14:17:57 +0100

    first commit

---
+++ finalproject/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  finalproject
+//
+//  Created by Bouznir on 15/02/2018.
+//  Copyright © 2018 Bouznir. All rights reserved.
+//
+
+import UIKit
+
+class LoginView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
